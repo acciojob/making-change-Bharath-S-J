44156dfd8e8db2ce21af1875ae45b08d3eff2fb3
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-05 21:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const makeChange = (c) => {
   // your name here
+	  const coins = [
+    ['q', 25],
+    ['d', 10],
+    ['n', 5],
+    ['p', 1],
+  ];
+
+	let res={}
+	for(let [char,coin] of coins){
+		let count=0
+		while(c>=coin){
+			c-=coin
+			count++
+		}
+		res[char]=count
+	}
+return res
+
+	
 };
 
 // Do not the change the code below
